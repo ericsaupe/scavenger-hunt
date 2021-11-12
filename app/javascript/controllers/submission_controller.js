883c459f58c3dc53f48c7c474613e38136062dd9
Eric Saupe <ericsaupe@gmail.com>
2021-11-12 13:07:32 -0700

    Reorder events for full submit lock

--- app/javascript/controllers/submission_controller.js
+++ app/javascript/controllers/submission_controller.js
@@ -4,12 +4,12 @@ export default class extends Controller {
   static targets = [ "form", "icon" ]
 
   submitForm(event) {
+    this.iconTarget.innerHTML = '<ion-icon name="cloud-upload-outline" class="animate__animated animate__bounce animate__infinite"></ion-icon>'
     if (this.formTarget.requestSubmit){
       this.formTarget.requestSubmit()
     } else {
       this.formTarget.submit()
     }
     event.target.disabled = true
-    this.iconTarget.innerHTML = '<ion-icon name="cloud-upload-outline" class="animate__animated animate__bounce animate__infinite"></ion-icon>'
   }
 }
