01062cd1ee35b8b439e907d01c19c4ebbf292bda
Eric Saupe <ericsaupe@gmail.com>
2021-11-29 11:22:58 -0800

    Disable form when timer expires

--- app/helpers/hunt_helper.rb
+++ app/helpers/hunt_helper.rb
@@ -5,12 +5,12 @@ module HuntHelper
     return if hunt.starts_at.nil? && hunt.ends_at.nil?
 
     text = if hunt.in_progress? && hunt.ends_at
-      "Ends in #{distance_of_time_in_words(Time.current, hunt.ends_at)}"
-    elsif hunt.upcoming?
-      "Starts in #{distance_of_time_in_words(Time.current, hunt.starts_at)}"
-    elsif hunt.ended?
-      "This scavenger hunt has ended. No more submissions will be accepted."
-    end
+             "Ends in #{distance_of_time_in_words(Time.current, hunt.ends_at)}"
+           elsif hunt.upcoming?
+             "Starts in #{distance_of_time_in_words(Time.current, hunt.starts_at)}"
+           elsif hunt.ended?
+             'This scavenger hunt has ended. No more submissions will be accepted.'
+           end
 
     content_tag(:span, text)
   end
