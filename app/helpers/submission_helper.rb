7e26cab2c31463215b51505c4f09004d7c4683b7
Eric Saupe <ericsaupe@gmail.com>
2021-12-03 14:45:36 -0800

    Update results styling

--- app/helpers/submission_helper.rb
+++ app/helpers/submission_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module SubmissionHelper
-  def submission_attachment_url(submission)
+  def submission_attachment_url(submission, variant_size: :small)
     return nil unless submission.photo.attached?
 
     if submission.video?
       url_for(submission.photo)
     else
-      url_for(submission.photo.variant(resize_to_fit: [500, 500]))
+      url_for(submission.photo.variant(variant_size))
     end
   end
 end
