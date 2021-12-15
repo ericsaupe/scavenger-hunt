770d9ae60e4b3a5e071c4e5417679358410deba4
Eric Saupe <ericsaupe@gmail.com>
2021-12-14 21:47:12 -0800

    tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js'
