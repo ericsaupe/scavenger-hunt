af382f2d71ddce71a78ce42ba0a23123625125dd
Eric Saupe <ericsaupe@gmail.com>
2022-11-21 11:51:06 -0700

    Fix #147

--- app/javascript/controllers/past_hunts_controller.js
+++ app/javascript/controllers/past_hunts_controller.js
@@ -9,18 +9,26 @@ export default class extends Controller {
   }
 
   connect() {
+    // Initialize localStorage
     if (localStorage.pastHunts === undefined) {
       localStorage.pastHunts = JSON.stringify({})
     }
+
     let pastHunts = JSON.parse(localStorage.pastHunts)
+    // Handle the list of past hunts
     if (this.hasListTarget && Object.keys(pastHunts).length !== 0) {
       Object.keys(pastHunts).forEach(hunt => {
+        // skip empty hunts or hunts that are already on the list
+        if (!hunt || this.listTarget.querySelector(`[value="${hunt}"]`)) {
+          return
+        }
         let opt = document.createElement('option')
         opt.value = hunt
         opt.innerHTML = pastHunts[hunt]
         this.listTarget.appendChild(opt)
       })
     } else {
+      // Handle storing joined hunts
       if (this.teamValue) {
         pastHunts[this.codeValue] = `${this.teamValue} - ${this.nameValue}`
       } else if(this.nameValue) {
