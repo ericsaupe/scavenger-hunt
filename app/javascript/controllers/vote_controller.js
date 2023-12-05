a5cb227d967abe3de1fd97f584ae12f398d2b2d9
Eric Saupe <ericsaupe@gmail.com>
2023-12-04 21:30:35 -0800

    Add confetti to vote menu

--- app/javascript/controllers/vote_controller.js
+++ app/javascript/controllers/vote_controller.js
@@ -1,14 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import { confetti } from "tsparticles-confetti"
 
 export default class extends Controller {
   static values = {
     submissionId: String,
+    value: String,
   }
 
   vote(event) {
     const submissionId = this.submissionIdValue
+    const currentValue = this.valueValue
     const value = event.target.dataset.value || event.target.parentElement.dataset.value
     const csrfToken = this.getMetaValue("csrf-token")
+    const button = event.target.closest("details") // it's actually a details element, not a button
 
     fetch(`/submissions/${submissionId}/votes`, {
       method: 'POST',
@@ -24,6 +28,44 @@ export default class extends Controller {
       }),
     }).then(r => r.text())
       .then(html => Turbo.renderStreamMessage(html))
+      .then(() => {
+        if (currentValue === value) {
+          return
+        }
+
+        const defaults = {
+          spread: 36,
+          ticks: 200,
+          gravity: 0,
+          decay: 0.94,
+          startVelocity: 10,
+          origin: {
+            x: (button.getBoundingClientRect().left + button.getBoundingClientRect().width / 2) / window.innerWidth,
+            y: button.getBoundingClientRect().top / window.innerHeight
+          },
+          particleCount: 20,
+          scalar: 4,
+          angle: 120,
+        }
+
+        if (value === "love") {
+          confetti({
+            ...defaults,
+            shapes: ["heart"],
+            colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+          })
+        } else if (value === "deny") {
+          confetti({
+            ...defaults,
+            shapes: ["text"],
+            shapeOptions: {
+              text: {
+                value: ["👎"],
+              },
+            },
+          })
+        }
+      })
   }
 
   getMetaValue(name) {
